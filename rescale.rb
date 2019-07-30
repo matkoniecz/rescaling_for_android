d0309ae96b0bb0ef21ac5c92b18dc77e263fc8d9
Mateusz Konieczny <matkoniecz@gmail.com>
2019-07-30 15:09:27 +0200

    refactor

--- rescale.rb
+++ rescale.rb
@@ -6,17 +6,21 @@ def rescale_square(input_file, output_path, size)
   `convert "#{input_file}" resize #{size}x#{size} "#{output_path}#{output_file}"`
 end
 
-def main
+def generate_drawable(output_path_root, input_filename)
   drawables = [
     { name: "drawable-mdpi", size: 128 },
     { name: "drawable-hdpi", size: 192 },
     { name: "drawable-xhdpi", size: 256 },
     { name: "drawable-xxhdpi", size: 384 },
   ]
-  output_path_root = "/home/mateusz/Documents/StreetComplete/app/src/main/res/"
   drawables.each do |drawable|
-    rescale_square(ARGV[0], output_path_root + drawable[:name], drawable[:size])
+    rescale_square(input_filename, output_path_root + drawable[:name], drawable[:size])
   end
+end
+
+def main
+  output_path_root = "/home/mateusz/Documents/StreetComplete/app/src/main/res/"
+  generate_drawable(output_path_root, ARGV[0])
 end
 
 main
