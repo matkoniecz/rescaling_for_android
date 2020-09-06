63ed88dd61f81ac73babfface07db6a0d71c85b4
Mateusz Konieczny <matkoniecz@gmail.com>
2020-09-06 10:54:29 +0200

    more readable error for nonexisting folder

--- rescale.rb
+++ rescale.rb
@@ -4,6 +4,9 @@
 
 def main
   output_path_root = "/home/mateusz/Documents/install_moje/StreetComplete/app/src/main/res/"
+  if Dir.exist?(output_path_root) == false
+    raise "nonexisting folder"
+  end
   Dir["*.{png,jpg,jpeg}"].each do |file|
     generate_drawable(file, output_path_root)
   end
